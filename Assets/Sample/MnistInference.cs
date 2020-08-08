98eb87a428017be25430d81f0f6eba2ae4b18686
Takuya Nakada <tky.play@gmail.com>
2020-08-08 22:47:45 +0900

    Sample

---
+++ Assets/Sample/MnistInference.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Barracuda;
+
+public class MnistInference
+{
+    readonly IWorker worker;
+    
+    public MnistInference(NNModel modelAsset)
+    {
+        var runtimeModel = ModelLoader.Load(modelAsset);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.CSharpBurst, runtimeModel);
+    }
+
+    //推論
+    //Mnistは28x28のfloat値(0~1)のinputで推論できる，左上が原点で右下に向かう座標系
+    public int Inference(float[] inputFloats)
+    {
+        //推論する
+        var scores = InferenceOnnx(inputFloats);
+
+        //最大のIndexを求める．Indexが推論した数字
+        var maxScore = float.MinValue;
+        int maxIndex = 0;
+        for (int i = 0; i < scores.Length; i++)
+        {
+            float score = scores[i];
+            if (maxScore < score)
+            {
+                maxScore = score;
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private float[] InferenceOnnx(float[] input)
+    {
+        var inputTensor = new Tensor(1, 28, 28, 1, input);
+        worker.Execute(inputTensor);
+        var outputTensor = worker.PeekOutput();
+        var outputArray = outputTensor.ToReadOnlyArray();
+        
+        inputTensor.Dispose();
+        outputTensor.Dispose();
+
+        return outputArray;
+    }
+
+    ~MnistInference()
+    {
+        worker.Dispose();
+    }
+}
---
+++ Assets/Sample/Paint.cs
@@ -0,0 +1,86 @@
+﻿using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paint : MonoBehaviour
+{
+    const int width = 28;
+    const int height = 28;
+
+    public MeshRenderer m;
+    public Button b;
+    public Text answer;
+    public NNModel modelAsset;
+    
+    private Texture2D texture;
+    private readonly Color[] buffer = new Color[width * height];
+    private readonly float[] input = new float[width * height];
+    private MnistInference mnistInference;
+
+    private void Start()
+    {
+        mnistInference = new MnistInference(modelAsset);
+        texture = new Texture2D(width, height);
+        m.material.mainTexture = texture;
+        ClearBuffer();
+    }
+
+    private void Update()
+    {
+        //クリック座標を白く塗りつぶす
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100.0f))
+            {
+                var pixel = new Vector2(hit.textureCoord.x * width, hit.textureCoord.y * height);
+                Draw(Vector2Int.RoundToInt(pixel));
+            }
+
+            texture.SetPixels(buffer);
+            texture.Apply();
+        }
+
+        //塗りつぶした部分を1としてfloatの配列に代入
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            input[i] = buffer[i].r;
+        }
+
+        //推論
+        var num = mnistInference.Inference(input);
+        answer.text = num.ToString();
+    }
+
+    //４ピクセル塗る
+    private void Draw(Vector2Int p)
+    {
+        DrawBuffer(p.x, p.y);
+        DrawBuffer(p.x + 1, p.y);
+        DrawBuffer(p.x + 1, p.y + 1);
+        DrawBuffer(p.x, p.y + 1);
+    }
+
+    //バッファーに書き込む
+    private void DrawBuffer(int x, int y)
+    {
+        if (x < 0 || width <= x || y < 0 || height <= y)
+        {
+            return;
+        }
+
+        buffer.SetValue(Color.white, x + width * y);
+    }
+
+    //黒(0)で塗りつぶしてクリア
+    public void ClearBuffer()
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = Color.black;
+        }
+
+        texture.SetPixels(buffer);
+        texture.Apply();
+    }
+}
